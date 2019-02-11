25c0e7b64495e224ce508521ea4a48d46d36b9c8
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-11 13:30:56 -0600

    login form renders on the /login endpoint

---
+++ oer-booker/src/components/Signin.js
@@ -0,0 +1,164 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+
+const ErrorMessage = styled.p`
+  color: red;
+`;
+
+const styles = theme => ({
+  main: {
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 2,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing.unit
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3
+  },
+  pushRight: {
+    marginLeft: 10
+  }
+});
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      errorMessage: null
+    };
+  }
+
+  handleChanges = e => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const endpoint =
+      'https://oer-bookr-api.herokuapp.com/books/login';
+    axios
+      .post(endpoint, {
+        username: this.state.username,
+        password: this.state.password
+      })
+      .then(res => {
+        console.log(res.data);
+        localStorage.setItem("jwt", res.data.token);
+        this.props.history.push("/workouts");
+      })
+      .catch(err => {
+        this.setState({ errorMessage: err.response.data.message });
+      });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <main className={classes.main}>
+        <CssBaseline />
+
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+
+          <form onSubmit={this.handleSubmit} className={classes.form}>
+            {this.state.errorMessage && (
+              <ErrorMessage>ERROR: {this.state.errorMessage}</ErrorMessage>
+            )}
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="username">Username</InputLabel>
+              <Input
+                type="text"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleChanges}
+                autoComplete="username"
+                autoFocus
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChanges}
+                type="password"
+                autoComplete="current-password"
+              />
+            </FormControl>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign in
+            </Button>
+            <div>
+              <p>
+                Don't have an account?
+                <span className={classes.pushRight}>
+                  <Link to="/sign-up">Sign Up</Link>
+                </span>
+              </p>
+            </div>
+          </form>
+        </Paper>
+      </main>
+    );
+  }
+}
+SignIn.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+export default withStyles(styles)(SignIn);
