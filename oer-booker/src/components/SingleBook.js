3c4a94476042aa9c1a1fc0ea1e9aed1418fd9e88
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-13 14:17:17 -0600

    add and delete review fully functional

--- oer-booker/src/components/SingleBook.js
+++ oer-booker/src/components/SingleBook.js
@@ -9,10 +9,10 @@ class SingleBook extends React.Component {
             books: [],
             reviews: [],
             singleReview: {
-               review: '',
-               reviewer: 'Megan',
-               rating: 3,
-               book_id: null
+                review: '',
+                reviewer: '',
+                rating: null,
+                book_id: null
             }
         }
     }
@@ -26,11 +26,25 @@ class SingleBook extends React.Component {
 
 
     handleReviewChanges = e => {
-        e.preventDefault();      
-        let singleReview = {...this.state.singleReview}
-        singleReview.review = e.target.value
-        this.setState({singleReview})
-      }
+      e.preventDefault();      
+      let singleReview = {...this.state.singleReview}
+      singleReview.review = e.target.value
+      this.setState({singleReview})
+    }
+
+    handleRatingChanges = e => {
+      e.preventDefault();      
+      let singleReview = {...this.state.singleReview}
+      singleReview.rating = e.target.value
+      this.setState({singleReview})
+    }
+
+    handleReviewerChanges = e => {
+      e.preventDefault();      
+      let singleReview = {...this.state.singleReview}
+      singleReview.reviewer = e.target.value
+      this.setState({singleReview})
+    }
   
 
       getReviews = () => {
@@ -65,6 +79,19 @@ class SingleBook extends React.Component {
           });
     }
 
+    deleteReview = (e, id) => {
+        e.preventDefault();
+        const endpoint =
+            `https://oer-bookr-api.herokuapp.com/reviews/${id}`;
+        axios
+          .delete(endpoint)
+          .then(res => {
+            console.log(res);
+            this.getReviews();
+          })
+          .catch( err => err.data)
+      }
+
 
     showModal = () => {
         this.setState({ show: true });
@@ -97,7 +124,7 @@ class SingleBook extends React.Component {
                             <p>{review.rating}</p>
                             <p>{review.review}</p>
                             <p>{review.reviewer}</p>
-                            <button>Delete</button>
+                            <button onClick={e => this.deleteReview(e, review.id)}>Delete</button>
                         
                             <Modal show={this.state.show} handleClose={this.hideModal}>
                                 <form>
@@ -107,6 +134,20 @@ class SingleBook extends React.Component {
                                         value={this.state.singleReview.review}
                                         name='review'
                                         onChange={this.handleReviewChanges}
+                                    />
+                                    <input
+                                        type='text'
+                                        placeholder='Add a rating'
+                                        value={this.state.singleReview.rating}
+                                        name='rating'
+                                        onChange={this.handleRatingChanges}
+                                    />
+                                    <input
+                                        type='text'
+                                        placeholder='Username'
+                                        value={this.state.singleReview.reviewer}
+                                        name='reviewer'
+                                        onChange={this.handleReviewerChanges}
                                     />
                                 </form>
                                 <button onClick={this.addReview}>Add</button>
